f9ec2ed36fae66a3bfc9850fb7e8f51dec88bedb
Anton Filatov <ya-enot@mail.ru>
2016-07-07 12:35:20 +0300

    Added logging removed exception throwing Minor logic fixes

--- src/main/java/org/filatov/springforest/ws/service/endpoint/mapping/JaxWsAnnotationMethodEndpointMapping.java
+++ src/main/java/org/filatov/springforest/ws/service/endpoint/mapping/JaxWsAnnotationMethodEndpointMapping.java
@@ -2,13 +2,11 @@ package org.filatov.springforest.ws.service.endpoint.mapping;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
-
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.xml.namespace.QName;
 import javax.xml.transform.TransformerFactory;
-
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.ws.context.MessageContext;
@@ -18,9 +16,7 @@ import org.springframework.ws.server.endpoint.support.PayloadRootUtils;
 @Component
 public class JaxWsAnnotationMethodEndpointMapping extends
 		AbstractAnnotationMethodEndpointMapping<QName> {
-
 	private static TransformerFactory transformerFactory;
-
 	static {
 		transformerFactory = TransformerFactory.newInstance();
 	}
@@ -35,69 +31,91 @@ public class JaxWsAnnotationMethodEndpointMapping extends
 	@Override
 	protected QName getLookupKeyForMethod(Method method) {
 		Class<?> webService = null;
-		Class<?>[] interfaces = method.getDeclaringClass().getInterfaces();
-		for (int i = 0; i < interfaces.length; i++) {
-			if (null != interfaces[i].getAnnotation(WebService.class)) {
-				webService = interfaces[i];
-			}
-		}
-		if (webService == null) {
-			throw new RuntimeException(
-					"@WebService was not found in hierarchy of: "
-							+ method.getDeclaringClass().getCanonicalName());
-		}
-		WebMethod webMethod = method.getAnnotation(WebMethod.class);
-		if (webMethod == null) {
-			try {
-				Method superMethod = webService.getMethod(method.getName(),
-						method.getParameterTypes());
-				System.out.println(superMethod);
-				webMethod = superMethod.getAnnotation(WebMethod.class);
-			} catch (NoSuchMethodException e) {
-				e.printStackTrace();
-			} catch (SecurityException e) {
-				e.printStackTrace();
-			}
-		}
-
-		Annotation[] paramAnnotations = null;
-		paramAnnotations = method.getParameterAnnotations()[0];
-		WebParam input = null;
-		while (null != paramAnnotations) {
-			for (int i = 0; i < paramAnnotations.length; i++) {
-				if (WebParam.class.isAssignableFrom(paramAnnotations[i]
-						.getClass())) {
-					input = (WebParam) paramAnnotations[i];
+		{
+			Class<?>[] interfaces = method.getDeclaringClass().getInterfaces();
+			for (int i = 0; i < interfaces.length; i++) {
+				if (null != interfaces[i].getAnnotation(WebService.class)) {
+					webService = interfaces[i];
+					break;
 				}
 			}
-			if (input == null) {
-				paramAnnotations = null;
+		}
+		if (webService != null) {
+			WebMethod webMethod = method.getAnnotation(WebMethod.class);
+			if (webMethod == null) {
 				try {
 					Method superMethod = webService.getMethod(method.getName(),
 							method.getParameterTypes());
-					paramAnnotations = superMethod.getParameterAnnotations()[0];
-				} catch (NoSuchMethodException e) {
-					e.printStackTrace();
-				} catch (SecurityException e) {
-					e.printStackTrace();
+					webMethod = superMethod.getAnnotation(WebMethod.class);
+				} catch (Throwable th) {
+					if (logger.isWarnEnabled()) {
+						logger.warn(
+								"@WebMethod annotation was not found on method or method declaration: "
+										+ method, th);
+					}
 				}
-
-			} else {
-				break;
 			}
-		}
-		if (webMethod != null && input != null) {
-			QName qname;
-			if (StringUtils.hasLength(input.partName())
-					&& StringUtils.hasLength(input.targetNamespace())) {
-				qname = new QName(input.targetNamespace(), input.partName());
-			} else {
-				qname = new QName(input.partName());
+			if (webMethod != null) {
+				WebParam inputType = null;
+				{
+					Annotation[][] paramsAnnotations = method
+							.getParameterAnnotations();
+					if (paramsAnnotations.length == 1) {
+						Annotation[] paramAnnotations = paramsAnnotations[0];
+						while (null != paramAnnotations) {
+							for (int i = 0; i < paramAnnotations.length; i++) {
+								if (WebParam.class
+										.isAssignableFrom(paramAnnotations[i]
+												.getClass())) {
+									inputType = (WebParam) paramAnnotations[i];
+								}
+							}
+							if (inputType == null) {
+								paramAnnotations = null;
+								try {
+									Method superMethod = webService.getMethod(
+											method.getName(),
+											method.getParameterTypes());
+									paramAnnotations = superMethod
+											.getParameterAnnotations()[0];
+								} catch (Throwable th) {
+									if (logger.isWarnEnabled()) {
+										logger.warn(
+												"@WebParam annotation was not found on method arguments or method declaration first argument: "
+														+ method, th);
+									}
+								}
+							} else {
+								break;
+							}
+						}
+					} else {
+						if (logger.isWarnEnabled()) {
+							logger.warn("@WebMethod annotated method or method declaration should have one @WebParam annotated argument: "
+									+ method);
+						}
+					}
+				}
+				if (inputType != null) {
+					QName qname;
+					if (StringUtils.hasLength(inputType.partName())
+							&& StringUtils.hasLength(inputType
+									.targetNamespace())) {
+						qname = new QName(inputType.targetNamespace(),
+								inputType.partName());
+					} else {
+						qname = new QName(inputType.partName());
+					}
+					return qname;
+				}
 			}
-			return qname;
 		} else {
-			return null;
+			if (logger.isErrorEnabled()) {
+				logger.error("@WebService annotation was not found in hierarchy of: "
+						+ method.getDeclaringClass());
+			}
 		}
+		return null;
 	}
 
 	@Override
